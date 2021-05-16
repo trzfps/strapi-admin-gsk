804bb0d4e7cd48a2fb5cb342b509c5cbf9814159
vitor <vitor.francisco@terceiro-sky.com.br>
2021-05-15 22:27:14 -0300

    changes

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -41,7 +41,7 @@ module.exports = {
       const token = strapi.admin.services.token.createJwtToken(user);
       ctx.cookies.set("token", token, {
         httpOnly: true,
-        secure: true,
+        secure: process.env.NODE_ENV === "production" ? true : false,
         maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
         domain: process.env.NODE_ENV === "development" ? "localhost" : process.env.PRODUCTION_URL,
       });
