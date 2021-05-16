d674eaf537a3d9d5f1e82c7e51f4271ddcfd12ff
vitor <vitor.francisco@terceiro-sky.com.br>
2021-05-16 17:18:43 -0300

    changes

--- admin/src/containers/Admin/Logout/index.js
+++ admin/src/containers/Admin/Logout/index.js
@@ -5,7 +5,7 @@
  */
 
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { withRouter } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -17,26 +17,11 @@ import Wrapper from './components';
 
 const Logout = ({ history: { push } }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [userInfo, setUserInfo] = useState(null);
-  const [loadingComplete, setLoadingComplete] = useState(false);
-    useEffect(
-        () => {
+  const [userInfo, setUserInfo] = useState(false);
 
-            const isLogin = async () => {
-                try {
-                  const requestUrl = "/admin/users/me";
-                  const result = await request(requestUrl, { method: 'GET' });
-                  console.log(result);
-                  setUserInfo(result.data);
-                } catch (e) {
-                    console.log(e);
-                }
-                setLoadingComplete(true);
-            }   
-            isLogin();
-        },
-        []
-    );
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
   const handleGoToMe = () => {
     push({
@@ -51,37 +36,39 @@ const Logout = ({ history: { push } }) => {
     push('/auth/login');
   };
 
-  const toggle = () => setIsOpen(prev => !prev);
-
-  
-  if(loadingComplete){
-    const displayName =
-      userInfo && userInfo.firstname && userInfo.lastname
-        ? `${userInfo.firstname} ${userInfo.lastname}`
-        : get(userInfo, 'username', '');
-
-    return (
-      <Wrapper>
-        <ButtonDropdown isOpen={isOpen} toggle={toggle}>
-          <DropdownToggle>
-            {displayName}
-            <FontAwesomeIcon icon="caret-down" />
-          </DropdownToggle>
-          <DropdownMenu className="dropDownContent">
-            <DropdownItem onClick={handleGoToMe} className="item">
-              <FormattedMessage id="app.components.Logout.profile" />
-            </DropdownItem>
-            <DropdownItem onClick={handleLogout}>
-              <FormattedMessage id="app.components.Logout.logout" />
-              <FontAwesomeIcon icon="sign-out-alt" />
-            </DropdownItem>
-          </DropdownMenu>
-        </ButtonDropdown>
-      </Wrapper>
-    );
+  const getUserInfo = async () => {
+    const requestURL = '/admin/users/me';
+    const result = await request(requestURL, { method: 'GET' });
+    setUserInfo(result.data);
 
   }
 
+  const toggle = () => setIsOpen(prev => !prev);
+
+  const displayName =
+    userInfo && userInfo.firstname && userInfo.lastname
+      ? `${userInfo.firstname} ${userInfo.lastname}`
+      : get(userInfo, 'username', '');
+
+  return (
+    <Wrapper>
+      <ButtonDropdown isOpen={isOpen} toggle={toggle}>
+        <DropdownToggle>
+          {displayName}
+          <FontAwesomeIcon icon="caret-down" />
+        </DropdownToggle>
+        <DropdownMenu className="dropDownContent">
+          <DropdownItem onClick={handleGoToMe} className="item">
+            <FormattedMessage id="app.components.Logout.profile" />
+          </DropdownItem>
+          <DropdownItem onClick={handleLogout}>
+            <FormattedMessage id="app.components.Logout.logout" />
+            <FontAwesomeIcon icon="sign-out-alt" />
+          </DropdownItem>
+        </DropdownMenu>
+      </ButtonDropdown>
+    </Wrapper>
+  );
 };
 
 export default withRouter(Logout);
